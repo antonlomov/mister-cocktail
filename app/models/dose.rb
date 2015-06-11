e0d2db2ccc11e0a821a42bcc20da5b7169619c29
anton lomov <anton.lomov@elcmarketing.com>
2015-06-11 15:40:54 +0200

    finished cocktail part

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,2 +1,8 @@
 class Dose < ActiveRecord::Base
+  belongs_to :cocktail
+  belongs_to :ingredient
+  validates :description, presence:true
+  validates :cocktail, presence:true
+  validates :ingredient, presence:true
+  validates :cocktail_id, uniqueness: {scope: :ingredient_id}
 end
